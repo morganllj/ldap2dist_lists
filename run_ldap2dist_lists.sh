b041cc4a50648b49bb125ba8063526c4871412f9
Morgan Jones <morgan@morganjones.org>
2016-12-09 18:27:43 -0500

    added run_ script

---
+++ run_ldap2dist_lists.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#
+# Morgan Jones (morgan@morganjones.org)
+# $Id$
+
+base_path=`echo $0 | awk -F/ '{for (i=1;i<NF;i++){printf $i "/"}}' | sed 's/\/$//'`
+log_path=${base_path}/log
+log=${log_path}/ldap2dist_lists_`date +%y%m%d.%H:%M:%S`
+
+l2z_cmd="${base_path}/ldap2dist_lists.pl $*"
+echo "** output logged to ${log}"
+echo
+echo $l2z_cmd
+$l2z_cmd 2>&1 | tee $log
+
